898a304135cc235a1655b752bc8876e6952410f4
Alessio Burgio <alessioburgio85@gmail.com>
2020-07-27 15:23:29 +0200

    Updates

---
+++ precache-manifest.d23648b5757d03360d8a1b32fee50a73.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f95b2096f3b949e0eb26c88c4202f59",
+    "url": "//aleshere.github.io/github-jobsearch-react/index.html"
+  },
+  {
+    "revision": "7a5ef0da3d2cf3435f85",
+    "url": "//aleshere.github.io/github-jobsearch-react/static/js/2.02e9321f.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "//aleshere.github.io/github-jobsearch-react/static/js/2.02e9321f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08b279074866c0294f82",
+    "url": "//aleshere.github.io/github-jobsearch-react/static/js/main.d124505b.chunk.js"
+  },
+  {
+    "revision": "e6e9b1cca424ebd80b15",
+    "url": "//aleshere.github.io/github-jobsearch-react/static/js/runtime-main.69adcc1c.js"
+  }
+]);
